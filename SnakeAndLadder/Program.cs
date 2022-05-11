bfb3b43f7cc25a5ff764cf75375ccf11e992f880
nk321626 <nk321626@gmail.com>
2022-05-11 18:52:53 +0530

    Added Start Game At Position 0 _UC1

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SnakeAndLadderProgram;
+
+Console.WriteLine("Welcome to Snake And Ladder Game");
+SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
+snakeAndLadder.StartGame();
+Console.ReadLine();
---
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SnakeAndLadderProgram
+{
+    public class SnakeAndLadder
+    {
+        private int PlayerPosition;
+        public void StartGame()
+        {
+            Console.WriteLine("Game Started");
+            Console.WriteLine("Player Position: {0} ", PlayerPosition);
+        }
+    }
+}
