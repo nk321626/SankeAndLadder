f4915a18498867ccc9655e3e93446bcb9f366d8b
nk321626 <nk321626@gmail.com>
2022-05-11 21:08:54 +0530

    Added Player Reaches the Winning Position 100 -UC4

--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -12,34 +12,72 @@ namespace SnakeAndLadderProgram
         public const int Ladder = 1;
         public const int snake = 2;
         private int PlayerPosition;
+        public const int Winning = 100;
+        int StartPoint = 0;
         public void StartGame()
         {
             Console.WriteLine("Game Started");
-            Console.WriteLine("Player Position: {0} ", PlayerPosition);
+            Console.WriteLine("Player Position:", PlayerPosition);
             Random random = new Random();
-            int DiceRolled = random.Next(1, 7);
-            Console.WriteLine("Dice Rolled : {0} ", DiceRolled);
-            PlayerPosition = PlayerPosition + DiceRolled;
-            Console.WriteLine("Player Position: {0}", PlayerPosition);
-            int Option = random.Next(0, 3);
-            Console.WriteLine("Dice Value :" + Option);
-            switch (Option)
+            //int DiceRolled = random.Next(1, 7);
+            //Console.WriteLine("Dice Rolled : {0} ", DiceRolled);
+            //PlayerPosition = PlayerPosition + DiceRolled;
+            //Console.WriteLine("Player Position: {0}", PlayerPosition);
+            //int Option = random.Next(0, 3);
+            //Console.WriteLine("Dice Value :" + Option);
+            //switch (Option)
+            //{
+            //    case No_Play:
+            //        Console.WriteLine("No Play");
+            //        break;
+            //        case Ladder:
+            //        PlayerPosition += DiceRolled;
+            //        Console.WriteLine("Got Ladder : " + PlayerPosition);
+            //        break;
+            //    case snake:
+            //        PlayerPosition -= DiceRolled;
+            //        Console.WriteLine("Snke Atteack : " + PlayerPosition);
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid Option");
+            //        break;
+            while (StartPoint < Winning)
             {
-                case No_Play:
-                    Console.WriteLine("No Play");
-                    break;
-                    case Ladder:
-                    PlayerPosition += DiceRolled;
-                    Console.WriteLine("Got Ladder : " + PlayerPosition);
-                    break;
-                case snake:
-                    PlayerPosition -= DiceRolled;
-                    Console.WriteLine("Snke Atteack : " + PlayerPosition);
-                    break;
-                default:
-                    Console.WriteLine("Invalid Option");
-                    break;
+                int DiceRolled = random.Next(1, 7);
+                int Option = random.Next(0, 3);
+                switch (Option)
+                {
+                    case No_Play:
+                        Console.WriteLine("No Play");
+                        break;
+                        case Ladder:
+                        StartPoint += DiceRolled;
+                        Console.WriteLine("Dice Rolls Value : +{0} ", DiceRolled);
+                        Console.WriteLine("Got Ladder : " + StartPoint);
+                        Console.WriteLine("Player Position : " + StartPoint);
+                        break;
+                        case snake:
+                        StartPoint -= DiceRolled;
+                        Console.WriteLine("Dice Rolls Value : -{0}", DiceRolled);
+                        Console.WriteLine("Snake Attack : " + StartPoint);
+
+                        if (StartPoint < 0)
+                        {
+                            StartPoint = 0;
+                        }
+                        Console.WriteLine("Player Position : " + StartPoint);
+                        break;
+                }
+                if(StartPoint >= Winning)
+                {
+                    Console.WriteLine("Won");
+
+                }
+            
             }
+            
         }
+        
     }
+    
 }
