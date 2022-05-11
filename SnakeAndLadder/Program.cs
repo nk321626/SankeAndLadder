90546322250e648c1ccab15a4cd7828c0545873a
nk321626 <nk321626@gmail.com>
2022-05-11 19:06:32 +0530

    Added Player rolls the Dice to get a Random number _UC2

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -3,5 +3,5 @@ using SnakeAndLadderProgram;
 
 Console.WriteLine("Welcome to Snake And Ladder Game");
 SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
-snakeAndLadder.StartGame();
+snakeAndLadder.DiceRolled();
 Console.ReadLine();
--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -9,10 +9,15 @@ namespace SnakeAndLadderProgram
     public class SnakeAndLadder
     {
         private int PlayerPosition;
-        public void StartGame()
+        public void DiceRolled()
         {
             Console.WriteLine("Game Started");
             Console.WriteLine("Player Position: {0} ", PlayerPosition);
+            Random random = new Random();
+            int DiceRolled = random.Next(1, 7);
+            Console.WriteLine("Dice Rolled : {0} ", DiceRolled);
+            PlayerPosition = PlayerPosition + DiceRolled;
+            Console.WriteLine("Player Position: {0}", PlayerPosition);
         }
     }
 }
